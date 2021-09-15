7b6605323037bb22be8a8567a6cf4c0c8a4fbdfe
Steven Smith <steven_smith@worksafe.vic.gov.au>
2021-09-15 22:54:05 +1000

    Wire up router and query strings

--- src/pages/search/search.tsx
+++ src/pages/search/search.tsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import SearchResults from "../../features/searchResults/searchResults";
 
-class Search extends Component {
+interface Props {
+    history: {
+        push(url: string): void;
+    };
+}
+
+class Search extends Component<Props> {
 
     state = {
         searchValue: '',
@@ -18,7 +24,7 @@ class Search extends Component {
                 <input name="text" type="text" className="rounded-xl shadow-md w-3/4 m-1 px-2 py-3" placeholder="Search"
                     onChange={event => this.handleOnChange(event)}
                     value={this.state.searchValue} />
-                <button type="submit" className="rounded-xl shadow-md bg-black text-white w-1/4 m-1 px-1 py-3" onClick={this.handleSearch}>Search</button>
+                <button type="submit" disabled={!this.state.searchValue} className="rounded-xl shadow-md bg-black disabled:bg-black-200 disabled:text-gray disabled:cursor-not-allowed text-white w-1/4 m-1 px-1 py-3" onClick={this.handleSearch}>Search</button>
                 </form>
             </div>
             {this.state.repos.length > 0 && (
@@ -29,13 +35,15 @@ class Search extends Component {
 
     handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         this.setState({ searchValue: event.target.value });
-        let currentUrlParams = new URLSearchParams(window.location.search);
-        currentUrlParams.set('page_num', this.state.pageNum.toString());
-        this.props.history.push(window.location.pathname + "?" + currentUrlParams.toString())
     };
 
     handleSearch = (e: any) => {
-        e.preventDefault()
+        e.preventDefault();
+        let currentUrlParams = new URLSearchParams(window.location.search);
+        currentUrlParams.set('q', this.state.searchValue);
+        currentUrlParams.set('page_num', this.state.pageNum.toString());
+        console.log('currentUrlParams', currentUrlParams.toString())
+        this.props.history.push(window.location.pathname + "?" + currentUrlParams.toString())
         this.fetchData();
     }
 
